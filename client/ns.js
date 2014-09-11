1a8aed866e941ee6f9a66541a07ba33b8546ecfc
Phil Cockfield <phil@cockfield.net>
2014-09-11 13:13:10 +1200

    Rename INTERNAL => PKG

--- client/ns.js
+++ client/ns.js
@@ -3,8 +3,6 @@
 
 /*
 PACAKGE LEVEL Scope.
-
 NOTE: Must be a JS file to share the variables at package-scope.
-      See Meteor documentation for more.
 */
-INTERNAL = {} // Internal namespace - scoped within package only.
+PKG = {}; // Internal package API - scoped within package only.
